b4a0b10ec8e699f38a2c544c421a92bba8b40b90
Dhruv Lathia <root@GreenCode>
2024-03-01 12:35:36 +0530

    Demo app hover to see details
    Created demo app from angular documentation tutorial and without routing, and with components, service, ngfor, 1 component to 2nd component data transfer and inject

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HousingService } from '../housing.service';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  imports: [CommonModule, HousingLocationComponent],
+  // templateUrl: './home.component.html',
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location *ngFor="let housingLocation of housingLocationList" [housingLocation]="housingLocation"></app-housing-location>
+    </section>
+  `,
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
 
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
---
+++ src/app/housinglocation.ts
@@ -0,0 +1,10 @@
+export interface HousingLocation {
+  id: number;
+  name: string; //used
+  city: string; //used
+  state: string; //used
+  photo: string; //used
+  availableUnits: number;
+  wifi: boolean;
+  laundry: boolean;
+}
